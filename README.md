fffa333ce54dd61e6f6a024865560c141bdf6474
N3rfed <jaden.delapaz@gmail.com>
2026-04-06 01:11:36 -0400

    ensured correctness and added comments to algo, question 1 on readme

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 Brianna Chua, 76131392
+Jaden Delapaz, 19812001
 
-# Assumptions
+# Requirements/Assumptions
+1. Python is installed
 
 
 # How to Run
-
+1. Using the terminal, enter src directory
+2. Run "python hvlcs.py tests/example.in"
+3. Output file will be found as a .out file
 
 # Question 1
+![alt text](src/q1.png)
 
+![alt text](src/runtime_graph.png)
 
 # Question 2
 
--- src/hvlcs.py
+++ src/hvlcs.py
@@ -4,36 +4,37 @@ def hvlcs(A, B, values):
     n, m = len(A), len(B)
     dp = []
 
-    for i in range(n + 1):
+    for i in range(n + 1): # Initialize array to 0. Includes empty string base case
         row = []
         for j in range(m + 1):
             row.append(0)
         dp.append(row)
 
-    for i in range(1, n + 1):
+    for i in range(1, n + 1): # Recurrence Relation loop
         for j in range(1, m + 1):
-            if A[i - 1] == B[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + values[A[i - 1]]
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            if A[i - 1] == B[j - 1]: # Check if character is a match in both strings
+                dp[i][j] = dp[i - 1][j - 1] + values[A[i - 1]] # Add value of the current character to the diagonal
+            else: # Character does not match
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) # Take best value if we ignore character in string A/B
 
+    # BACKTRACKING / POST-PROCESSING
     i, j = n, m
-    subseq = []
+    subseq = [] # Holds characters as we go through table
 
-    while i > 0 and j > 0:
-        if A[i - 1] == B[j - 1]:
-            subseq.append(A[i - 1])
+    while i > 0 and j > 0: # Iterate backwards
+        if A[i - 1] == B[j - 1]: # Check if characters are the same
+            subseq.append(A[i - 1]) # Add character to the array
             i -= 1
             j -= 1
-        elif dp[i - 1][j] > dp[i][j - 1]:
-            i -= 1
-        else:
-            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]: # Check neighboring elements (up or left)
+            i -= 1 # Move up
+        else: 
+            j -= 1 # Move left
     
-    subseq.reverse()
+    subseq.reverse() # Reverse string since we started at the end
     return dp[n][m], ''.join(subseq)
 
-def read_input(filename):
+def read_input(filename): # Reading input as outlined in the spec
     with open(filename, 'r') as f:
         K = int(f.readline().strip())
         values = {}
